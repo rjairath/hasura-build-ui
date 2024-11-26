010d14bd34b39dd2b49fdacf643ea9b565b45b25
Matthew Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2024-11-26 11:50:16 -0600

    install bun / mantine

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
+
 const queryClient = new QueryClient();
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
