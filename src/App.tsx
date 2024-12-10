39537a5e3d611b0a78ace59b5807e27c83acbb11
Rishabh Jairath <rishabhjairath@gmail.com>
2024-12-11 01:30:10 +0530

    notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {
   Alert,
+  Button,
   Center,
   Container,
   Group,
@@ -7,6 +8,7 @@ import {
   Paper,
   Stack,
   Title,
+  Notification
 } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { CharacterSelector } from "./components/CharacterSelector";
@@ -26,6 +28,7 @@ function App() {
   const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(null);
   const [selectedPlanet, setSelectedPlanet] = useState<Planet | null>(null);
   const [selectedShip, setSelectedShip] = useState<Starship | null>(null);
+  const [showNotification, setShowNotification] = useState(false);
 
   if (isPending) {
     return <LoadingOverlay visible />;
@@ -40,33 +43,74 @@ function App() {
   }
 
   return (
-    <Container fluid maw={1200} my={"xl"}>
-      <Stack>
-        <Paper withBorder p={"xl"} component={Stack} gap={"xl"} radius={"md"}>
-          <Title order={1}>Galactic Datapad</Title>
-          <Group justify="space-between" align="flex-start" style={{ width: "100%", gap: "1rem" }}>
-            {/* Feature components: */}
-            <CharacterSelector 
-              characters={data.people} selectedCharacter={selectedCharacter} 
-              setSelectedCharacter={setSelectedCharacter}
-            />
-            <PlanetSelector 
-              planets={data.planets} selectedPlanet={selectedPlanet}
-              setSelectedPlanet={setSelectedPlanet}
-            />
-            <StarshipSelector 
-              pilotedStarships={selectedCharacter?.starshipConnection?.starships}
-              allStarships={data.starships}
-              selectedShip={selectedShip}
-              setSelectedShip={setSelectedShip}
-            />
-          </Group>
-        </Paper>
-        <Paper withBorder p={"xl"} radius={"md"}>
-          <MissionSummary starship={selectedShip} planet={selectedPlanet} character={selectedCharacter} />
-        </Paper>
-      </Stack>
-    </Container>
+      <Container fluid maw={1200} my={"xl"}>
+          <Stack style={{position: "relative"}}>
+              {showNotification && (
+                  <Notification
+                      style={{ position: "absolute", right: "0"}}
+                      radius={"md"}
+                      title="Mission Alert"
+                      color="blue"
+                      onClose={() => setShowNotification(false)}
+                  >
+                      May the force be with you!!
+                  </Notification>
+              )}
+              <Paper
+                  withBorder
+                  p={"xl"}
+                  component={Stack}
+                  gap={"xl"}
+                  radius={"md"}
+              >
+                  <Title order={1}>Galactic Datapad</Title>
+                  <Group
+                      justify="space-between"
+                      align="flex-start"
+                      style={{ width: "100%", gap: "1rem" }}
+                  >
+                      {/* Feature components: */}
+                      <CharacterSelector
+                          characters={data.people}
+                          selectedCharacter={selectedCharacter}
+                          setSelectedCharacter={setSelectedCharacter}
+                      />
+                      <PlanetSelector
+                          planets={data.planets}
+                          selectedPlanet={selectedPlanet}
+                          setSelectedPlanet={setSelectedPlanet}
+                      />
+                      <StarshipSelector
+                          pilotedStarships={
+                              selectedCharacter?.starshipConnection?.starships
+                          }
+                          allStarships={data.starships}
+                          selectedShip={selectedShip}
+                          setSelectedShip={setSelectedShip}
+                      />
+                  </Group>
+                  <Button
+                      style={{ marginLeft: "auto" }}
+                      size="md"
+                      variant="gradient"
+                      gradient={{ from: "teal", to: "blue" }}
+                      disabled={
+                          !selectedCharacter || !selectedPlanet || !selectedShip
+                      }
+                      onClick={() => setShowNotification(true)}
+                  >
+                      Embark on Mission
+                  </Button>
+              </Paper>
+              <Paper withBorder p={"xl"} radius={"md"}>
+                  <MissionSummary
+                      starship={selectedShip}
+                      planet={selectedPlanet}
+                      character={selectedCharacter}
+                  />
+              </Paper>
+          </Stack>
+      </Container>
   );
 }
 
